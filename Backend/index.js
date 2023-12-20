1ef5db2f90012d607415eeb6d8bf6e0464168545
Binuki Mihara <133636115+BinukiMihara@users.noreply.github.com>
2023-12-20 16:12:09 +0530

    Update a book with mongoose

--- Backend/index.js
+++ Backend/index.js
@@ -14,7 +14,7 @@ app.get("/", (request, response) => {
 });
 
 //Route to save a new book
-app.post(`/books`, async (request, response) => {
+app.post("/books", async (request, response) => {
   try {
     if (
       !request.body.title ||
@@ -60,6 +60,32 @@ app.get("/books/:id", async (request, response) => {
     const book = await Book.findById(id);
 
     return response.status(200).json(book);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+//route to update a book
+app.put("/books/:id", async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response.status(400).send({
+        message: `Send all the reuired fields: title,author,publishYear`,
+      });
+    }
+    const { id } = request.params;
+    const result = await Book.findByIdAndUpdate(id, request.body);
+
+    if (!result) {
+      return response.status(404).json({ message: `Book Not Found` });
+    }
+
+    return response.status(200).send({ message: `Book Updates Successfully!` });
   } catch (error) {
     console.log(error.message);
     response.status(500).send({ message: error.message });
