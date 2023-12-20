9bb9e216b80dfd21c1f7b49d64b54119ebca7735
Binuki Mihara <133636115+BinukiMihara@users.noreply.github.com>
2023-12-20 18:12:23 +0530

    Delete a book with mongoose

--- Backend/index.js
+++ Backend/index.js
@@ -91,6 +91,24 @@ app.put("/books/:id", async (request, response) => {
     response.status(500).send({ message: error.message });
   }
 });
+
+//route to delete a book
+app.delete("/books/:id", async (request, response) => {
+  try {
+    const { id } = request.params;
+    const result = await Book.findByIdAndDelete(id);
+
+    if (!result) {
+      return response.status(404).json({ message: `Book Not Found` });
+    }
+
+    return response.status(200).send({ message: `Book Deleted Successfully!` });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
 mongoose
   .connect(mongoDBURL)
   .then(() => {
