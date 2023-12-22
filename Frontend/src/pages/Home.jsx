7748a72a68da58df328311edb15f3d14704e7ac4
Binuki Mihara <133636115+BinukiMihara@users.noreply.github.com>
2023-12-22 23:47:52 +0530

    CSS changed

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -36,11 +36,11 @@ const Home = () => {
         <table className='w-full border-separate border-spacing-2'>
           <thead>
             <tr>
-              <th className='border border- bg-slate-600 rounded-md'>No</th>
-              <th className='border border- bg-slate-600 rounded-md'>Title</th>
-              <th className='border border- bg-slate-600 rounded-md max-md:hidden'>Author</th>
-              <th className='border border- bg-slate-600 rounded-md max-md:hidden'>Publish Year</th>
-              <th className='border border- bg-slate-600 rounded-md'>Operations</th>
+              <th className='border border- border-slate-600 rounded-md'>No</th>
+              <th className='border border- border-slate-600 rounded-md'>Title</th>
+              <th className='border border- border-slate-600 rounded-md max-md:hidden'>Author</th>
+              <th className='border border- border-slate-600 rounded-md max-md:hidden'>Publish Year</th>
+              <th className='border border- border-slate-600 rounded-md'>Operations</th>
 
             </tr>
 
@@ -48,11 +48,11 @@ const Home = () => {
           <tbody>
             {books.map((book, index) => (
               <tr key={book._id} className='h-8'>
-                <td className='border border- bg-slate-700 rounded-md text-center'>{index+1}</td>
-                <td className='border border- bg-slate-700 rounded-md text-center'>{book.title}</td>
-                <td className='border border- bg-slate-700 rounded-md text-center max-md:hidden'>{book.author}</td>
-                <td className='border border- bg-slate-700 rounded-md text-center max-md:hidden'>{book.publishYear}</td>
-                <td className='border border- bg-slate-700 rounded-md text-center'>
+                <td className='border border- border-slate-700 rounded-md text-center'>{index+1}</td>
+                <td className='border border- border-slate-700 rounded-md text-center'>{book.title}</td>
+                <td className='border border- border-slate-700 rounded-md text-center max-md:hidden'>{book.author}</td>
+                <td className='border border- border-slate-700 rounded-md text-center max-md:hidden'>{book.publishYear}</td>
+                <td className='border border- border-slate-700 rounded-md text-center'>
                   <div className='flex justify-center gap-x-4'>
                     <Link to={`/books/details/${book._id}`}>
                       <BsInfoCircle className='text-2xl text-green-800'/>
